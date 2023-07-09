2625b2e9078053b158de68ade1caa604c8218402
ShashwatPrak <133594982+ShashwatPrak@users.noreply.github.com>
2023-07-09 22:00:30 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Admin_info,Ta_info,Student_info
+from .models import Admin_info,Ta_info,Student_info, Courses, Announcements, Announcement_follow, Discussions, Assignments
 
 class Admin_infoSerializer(ModelSerializer):
     class Meta:
@@ -14,4 +14,33 @@ class Ta_infoSerializer(ModelSerializer):
 class Student_infoSerializer(ModelSerializer):
     class Meta:
         model =  Student_info
+        fields = '__all__'
+
+class CoursesSerializer(ModelSerializer):
+    class Meta:
+        model = Courses
+        fields = '__all__'
+
+
+class AnnouncementsSerializer(ModelSerializer):
+    class Meta:
+        model = Announcements
+        fields = '__all__'
+
+
+class Announcement_followSerializer(ModelSerializer):
+    class Meta:
+        model = Announcement_follow
+        fields = '__all__'
+
+
+class DiscussionsSerializer(ModelSerializer):
+    class Meta:
+        model = Discussions
+        fields = '__all__'
+
+
+class AssignmentsSerializer(ModelSerializer):
+    class Meta:
+        model = Assignments
         fields = '__all__'
